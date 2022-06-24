99ce4b7f008f0baf5ffc002aa97f81640e674940
me-naresh141 <me.naresh141@gmail.com>
2022-06-24 14:53:01 +0530

    text changes

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ data.books.forEach((book) => {
   let img = document.createElement('img')
   img.classList.add('image')
   img.src = book.image
+  img.alt = book.title
   let title = document.createElement('h2')
   title.setAttribute('class', 'title')
   title.innerText = book.title
   let author = document.createElement('p')
   author.classList.add('authorName')
-  author.innerText = book.author
+  author.innerText = 'Author : '
+  let span = document.createElement('span')
+  span.innerText = book.author
   let button = document.createElement('button')
   button.className = 'btn'
   button.innerText = 'Buy now'
+  author.append(span)
   li.append(img, title, author, button)
   ul.append(li)
+  console.log(li)
 })
